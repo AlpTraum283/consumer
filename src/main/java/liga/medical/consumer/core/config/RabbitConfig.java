019fc83ab283e0ae58b9dc8e6e97675af3848fd0
alptraum283 <alptraum283@gmail.com>
2022-10-25 14:27:54 +0300

    config changes

--- src/main/java/liga/medical/consumer/core/config/RabbitConfig.java
+++ src/main/java/liga/medical/consumer/core/config/RabbitConfig.java
@@ -5,18 +5,28 @@ import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.logging.Logger;
-
 @Configuration
 public class RabbitConfig {
-    Logger logger = Logger.getLogger(String.valueOf(RabbitConfig.class));
+
+    @Value("${spring.rabbitmq.host}")
+    private String localhost;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
 
     @Bean
     public ConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory("localhost");
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(localhost);
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+        return cachingConnectionFactory;
     }
 
     @Bean
